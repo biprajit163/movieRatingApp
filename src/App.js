7e3ac5ee5b294606e362ea659069e7ef8f1bd72b
biprajit163 <biprajit163@gmail.com>
2021-01-15 12:17:30 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home.js'
 function App() {
   return (
     <div className="App">
-      <h1>Rate Star Wars Movies</h1>
+      <h1>Rate Starwars Movies</h1>
       <Home/>
     </div>
   );

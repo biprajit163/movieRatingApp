2efa1bf1b8a7d67e81f473330134615d3ef074d3
biprajit163 <biprajit163@gmail.com>
2021-01-15 20:50:52 -0500

    style info page

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+
+
 function MovieInfo({ match }) {
 
     const [movie, setMovie] = useState({})
@@ -56,10 +58,58 @@ function MovieInfo({ match }) {
     }
 
     return (
-        <div>
+        <div className="MovieInfo">
             <h3>{movie.title}</h3>
             <p>Episode Number: {movie.episode_id}</p>
             <p>Description: {movie.opening_crawl}</p>
+
+            <div className="movie-features-container">
+                <div className="character">
+                    <h4>{character.name}</h4>
+                    <p>Height: {character.height}</p>
+                    <p>Mass: {character.mass}</p>
+                    <p>Gender: {character.gender}</p>
+                    <p>Hair Color: {character.hair_color}</p>
+                    <p>Skin Color: {character.skin_color}</p>
+                    <p>Eye Color: {character.eye_color}</p>
+                    <p>Birth Year: {character.birth_year}</p>
+                </div>
+
+                <div className="planet">
+                    <h4>{planet.name}</h4>
+                    <p>Rotation Period: {planet.rotation_period} hrs</p>
+                    <p>Orbital Period: {planet.orbital_period} days</p>
+                    <p>Planet Diameter: {planet.diameter}</p>
+                    <p>Climate: {planet.climate}</p>
+                    <p>Gravity: {planet.gravity}</p>
+                    <p>Planet Terrain: {planet.terrain}</p>
+                    <p>Population: {planet.population}</p>
+                </div>
+
+                <div className="starship">
+                    <h4>{starship.name}</h4>
+
+                    <div className="starship-card">
+                        <div className="starship-info">
+                            <p>Model: {starship.model}</p>
+                            <p>manufacturer: {starship.manufacturer}</p>
+                            <p>Cost: {starship.cost_in_credits} credits</p>
+                            <p>Starship Class: {starship.starship_class}</p>
+                        </div>
+
+                        <div className="starship-details">
+                            <p>Starship Details:</p>
+                            <p>Length: {starship.length}</p>
+                            <p>Max Atmosphere Speed: {starship.max_atmosphere_speed}</p>
+                            <p>Crew: {starship.crew}</p>
+                            <p>Passenger capacity: {starship.passengers}</p>
+                            <p>Cargo Capacity: {starship.cargo_capacity}</p>
+                            <p>Hyperdrive Rating: {starship.hyperdrive_rating} unit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }

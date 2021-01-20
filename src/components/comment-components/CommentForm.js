12dc2fe11ea39fd3f03f43c9e21d6f96e591e2d4
biprajit163 <biprajit163@gmail.com>
2021-01-19 21:03:16 -0500

    <CommentList/> styling

--- src/components/comment-components/CommentForm.js
+++ src/components/comment-components/CommentForm.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-
+import { FaAtom } from 'react-icons/fa';
 
 function CommentForm({ addComment }) {
 
@@ -18,16 +18,29 @@ function CommentForm({ addComment }) {
     
     return (
         <form onSubmit={handleSubmit} className="comment-form">
-            <label htmlFor="comment">Make a comment:</label>
+            {/* <label htmlFor="comment">Make a comment:</label> */}
             <textarea 
+                placeholder="Add a comment..."
                 id="comment" 
                 type="text" 
                 value={comment}
                 onChange={handleChange}
-                cols="10"
-                rows="5"
+                cols="100"
+                rows="2"
             ></textarea>
-            <button type="submit" className="comment-form-button">Comment</button>
+            <button type="submit" className="comment-form-button"
+                style={{
+                    width: '80px',
+                    height: 'auto'
+                }}
+            >
+                <FaAtom
+                    style={{
+                        width: '100%',
+                        height: 'auto'
+                    }}
+                />
+            </button>
         </form>
     );
 }
